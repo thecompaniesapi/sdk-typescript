317d9ba1fbb8a0a651477cf3de3d485ac50ff2af
Yaël GUILLOUX <yael.guilloux@gmail.com>
2025-07-17 02:18:27 +0200

    feat(sdk-typescript): cleanup & simplify ci

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,7 @@ export default antfu(
   {
     type: 'lib',
     ignores: ['src/openapi.ts'],
-    pnpm: true,
     rules: {
-      'pnpm/json-enforce-catalog': 'off',
       'ts/explicit-function-return-type': 'off',
     },
   },
